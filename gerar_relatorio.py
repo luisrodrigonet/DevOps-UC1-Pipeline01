281105657ce6111555b06dafff27e871dcced4fa
Luís Rodrigo de Oliveira Gonçalves <luis.rodrigo.net@gmail.com>
2025-03-20 12:37:21 -0300

    Atualizacao do arquivo de dados, inclusão do script para gerar o relatório e arquivo ci.yml

---
+++ gerar_relatorio.py
@@ -0,0 +1,9 @@
+import pandas as pd  
+
+df = pd.read_csv('data.csv')  
+relatorio = df.describe().to_html()  
+
+with open('relatorio.html', 'w') as f:  
+    f.write(f"<h1>Relatório de Dados</h1>\n{relatorio}")
+
+    
